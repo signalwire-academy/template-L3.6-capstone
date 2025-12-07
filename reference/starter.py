ea22025b6f348aa502427fd80c2761875e5afc81
Brian West <brian@signalwire.com>
2025-12-07 05:45:49 -0800

    Add starter template

---
+++ reference/starter.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""Lab 3.6: Capstone Project - Starter Template
+
+Complete the TODOs to implement the lab requirements.
+"""
+
+from signalwire_agents import AgentBase, SwaigFunctionResult
+
+# TODO: Create your agent here
+
+if __name__ == "__main__":
+    # TODO: Run your agent
+    pass
